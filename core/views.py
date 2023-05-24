fc4822c0f6f915d54c4d76023cb01a483ae052ab
Oleg Babin <babin.architect@gmail.com>
2023-05-24 13:36:07 +0300

    update views

--- core/views.py
+++ core/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 
-from currency.services import ExchangeRatesService
+from currency.models import ExchangeRateProvider
+from currency.services import ExchangeRatesService, ProviderService
 
 
 # Create your views here.
 def index(request):
-    service = ExchangeRatesService()
+    providers = {
+        'name': "Privat Bank",
+        'api_url': "https://api.privatbank.ua/p24api/exchange_rates"
+    }
+    # url = "https://api.privatbank.ua/p24api/exchange_rates"
+    # name = "Privat Bank"
+
+    # provider = ProviderService(name=providers['name'], api_url=providers['api_url']).get_or_create()
+    # print(provider)
+    service = ExchangeRatesService(name=providers['name'], api_url=providers['api_url'])
     rates = service.get_rates()
-    print(rates)
     return render(request, 'core/index.html')
