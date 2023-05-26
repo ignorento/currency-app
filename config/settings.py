c6bf579193e62b642ad5a2f4a194610f13cdbd52
Oleg Babin <babin.architect@gmail.com>
2023-05-26 03:01:45 +0300

    update settings.py

--- config/settings.py
+++ config/settings.py
@@ -127,3 +127,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Нужно сделать .env
+PROVIDERS = {
+        'name': "Privat Bank",
+        'api_url': "https://api.privatbank.ua/p24api/exchange_rates"}
