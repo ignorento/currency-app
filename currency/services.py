1f8813dc2278209b1510a8e3947dfd908ca45a93
Oleg Babin <babin.architect@gmail.com>
2023-05-30 02:02:27 +0300

    update

--- currency/services.py
+++ currency/services.py
@@ -17,14 +17,6 @@ class ProviderService:
         return provider
 
 
-
-
-
-
-
-
-
-
 class ExchangeRatesService:
 
     CURRENCIES = ['GBP', 'USD', 'CHF', 'EUR']
@@ -37,7 +29,6 @@ class ExchangeRatesService:
         self.api_url = api_url
         self.provider = ProviderService(**self.__dict__).get_or_create()
 
-
     def process_date(self, date):
         # Ваш код для обработки каждой даты
         if ExchangeRatesService.date_check(date):
@@ -48,33 +39,29 @@ class ExchangeRatesService:
             return currency_rates
         else:
             print(f'exchange rate for the {date} date is already in the database')
-
+            return None
 
     def get_rates(self):
         start_test = datetime.datetime.now()
         start_date = datetime.datetime(2023, 5, 11)
         end_date = datetime.datetime.now()
 
-        dates = [
-            (start_date + datetime.timedelta(days=i)).strftime("%d.%m.%Y")
-            for i in range((end_date - start_date).days + 1)
-        ]
+        # dates = [
+        #     (start_date + datetime.timedelta(days=i)).strftime("%d.%m.%Y")
+        #     for i in range((end_date - start_date).days + 1)
+        # ]
 
         # Создаем пул потоков с ThreadPoolExecutor
         with ThreadPoolExecutor(max_workers=5) as executor:
-            # Запускаем задачи обработки дат в пуле потоков
-            futures = [executor.submit(self.process_date, date) for date in dates]
-            # Получаем результаты выполнения задач
-            for future in concurrent.futures.as_completed(futures):
+
+            while start_date < end_date:
+                start_date_format = start_date.strftime("%d.%m.%Y")
+
+                future = executor.submit(self.process_date, start_date_format)
+                # Получаем результаты выполнения задач
+
                 currency_rates = future.result()
                 if currency_rates:
-        # while start_date < end_date:
-        #     start_date_format = start_date.strftime("%d.%m.%Y")
-
-            ## if self.date_check(start_date_format):
-                # future = executor.submit(self.get_rate, start_date_format)
-                ## currency_rates = self.get_rate(date=start_date_format)
-                # currency_rates = future.result()
                     self.add_to_db(currency_rates)
                     print(currency_rates)
 
@@ -99,7 +86,7 @@ class ExchangeRatesService:
             # else:
             #     print(f'exchange rate for the {start_date_format} date is already in the database')
             #
-            # start_date += datetime.timedelta(days=1)
+                start_date += datetime.timedelta(days=1)
 
         sorted_db_by_time = ExchangeRate.objects.all().order_by('date').values()
         end_test = datetime.datetime.now()
