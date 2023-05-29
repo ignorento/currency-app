60fdc85aaab842c7437f0f34f7786fd4a77d40bd
Oleg Babin <babin.architect@gmail.com>
2023-05-30 00:54:17 +0300

    update

--- currency/services.py
+++ currency/services.py
@@ -1,3 +1,6 @@
+import concurrent
+from concurrent.futures import ThreadPoolExecutor
+
 import requests
 import datetime
 
@@ -19,28 +22,58 @@ class ExchangeRatesService:
     CURRENCIES = ['GBP', 'USD', 'CHF', 'EUR']
 
     # очищает базу перед парсингом и записью в базу
-    # ExchangeRate.objects.all().delete()
+    ExchangeRate.objects.all().delete()
 
     def __init__(self, name, api_url):
         self.name = name
         self.api_url = api_url
         self.provider = ProviderService(**self.__dict__).get_or_create()
 
+
+    def process_date(self, date):
+        # Ваш код для обработки каждой даты
+        if ExchangeRatesService.date_check(date):
+            currency_rates = self.get_rate(date=date)
+            if isinstance(currency_rates, str):
+                print(currency_rates)
+                return None
+            return currency_rates
+        else:
+            print(f'exchange rate for the {date} date is already in the database')
+
+
     def get_rates(self):
-        start_date = datetime.datetime(2023, 5, 14)
+        start_test = datetime.datetime.now()
+        start_date = datetime.datetime(2023, 5, 11)
         end_date = datetime.datetime.now()
 
-        while start_date < end_date:
-            start_date_format = start_date.strftime("%d.%m.%Y")
+        dates = [
+            (start_date + datetime.timedelta(days=i)).strftime("%d.%m.%Y")
+            for i in range((end_date - start_date).days + 1)
+        ]
 
-            if not self.date_check(start_date_format):
-                currency_rates = self.get_rate(date=start_date_format)
-                print(currency_rates)
+        # Создаем пул потоков с ThreadPoolExecutor
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            # Запускаем задачи обработки дат в пуле потоков
+            futures = [executor.submit(self.process_date, date) for date in dates]
+            # Получаем результаты выполнения задач
+            for future in concurrent.futures.as_completed(futures):
+                currency_rates = future.result()
+                if currency_rates:
+        # while start_date < end_date:
+        #     start_date_format = start_date.strftime("%d.%m.%Y")
 
-                if isinstance(currency_rates, str):
-                    break
+            ## if self.date_check(start_date_format):
+                # future = executor.submit(self.get_rate, start_date_format)
+                ## currency_rates = self.get_rate(date=start_date_format)
+                # currency_rates = future.result()
+                    self.add_to_db(currency_rates)
+                    print(currency_rates)
 
-                self.add_to_db(currency_rates)
+                # if isinstance(currency_rates, str):
+                #     break
+
+                # self.add_to_db(currency_rates)
                 # exchange_rates = [
                 #     ExchangeRate(
                 #         base_currency=item['base_currency'],
@@ -55,12 +88,15 @@ class ExchangeRatesService:
                 #
                 # ExchangeRate.objects.bulk_create(exchange_rates)
 
-            else:
-                print(f'exchange rate for the {start_date_format} date is already in the database')
-
-            start_date += datetime.timedelta(days=1)
+            # else:
+            #     print(f'exchange rate for the {start_date_format} date is already in the database')
+            #
+            # start_date += datetime.timedelta(days=1)
 
         sorted_db_by_time = ExchangeRate.objects.all().order_by('date').values()
+        end_test = datetime.datetime.now()
+        execution_time = end_test - start_test
+        print(f"Время выполнения программы: {execution_time}")
         return sorted_db_by_time
 
     def get_rate(self, date=None):
@@ -109,9 +145,8 @@ class ExchangeRatesService:
             .first()
         )
         if my_date_in_db:
-            return True
-        else:
             return False
+        return True
 
     @staticmethod
     def add_to_db(currency_rates):
---
+++ threading_sample.py
@@ -0,0 +1,47 @@
+import concurrent.futures
+import threading
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+
+def task(x):
+    thread_id = threading.get_ident()
+    print(f"Task {x} executed by Thread {thread_id % 100}\n")
+    time.sleep(5)
+
+    result = x * x
+    print(f"Execute {thread_id % 100} task: {result}")
+    print(f"_________{thread_id % 100}_____________")
+    return result
+
+def fast_task():
+    time.sleep(2)
+    print("Execute fast task")
+
+
+class ThreadService:
+    def instance_task(self):
+        print("Execute Class")
+
+
+if __name__ == "__main__":
+
+    # for i in range(5):
+    #     thread = threading.Thread(target=task, args=(i, ))
+    #     thread.start()
+
+    # thread = threading.Thread(target=fast_task)
+    # thread.start()
+    #
+    # service = ThreadService()
+    # t = threading.Thread(target=service.instance_task)
+    # t.start()
+
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        futures = [executor.submit(task, x=i) for i in range(10)]
+
+        for future in concurrent.futures.as_completed(futures):
+            result = future.result()
+            print(f"Resalt: {result}")
+            print(' \n')
+    print("Done!")
